2b547563acc02759495e8a9b42fd2c8aa8c68336
Sanjitha <sanjitha@example.com>
2021-03-20 23:16:56 -0700

    Website in Flask need to still put enryption and decryption

---
+++ migrations/versions/d22d1c840342_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: d22d1c840342
+Revises: 
+Create Date: 2021-03-20 22:58:14.070402
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd22d1c840342'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
