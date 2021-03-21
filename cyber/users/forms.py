fdcde1278a7982053654a79fd03b805737d28f8c
Sanjitha <sanjitha@example.com>
2021-03-21 01:18:41 -0700

    encryption done

--- cyber/users/forms.py
+++ cyber/users/forms.py
@@ -1,3 +1,4 @@
+import os
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired,Email,EqualTo
@@ -8,7 +9,14 @@ from flask_login import current_user
 from cyber.models import User
 from CyberSecurity import cyberSecurity
 
+global aes
+
 class LoginForm(FlaskForm):
+    global aes
+
+    aes = cyberSecurity("secret")
+    print("object")
+    #print(aes)
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Log In')
@@ -31,14 +39,22 @@ class RegistrationForm(FlaskForm):
 
 
 class UpdateUserForm(FlaskForm):
+    global aes
     email = StringField('Email', validators=[DataRequired(),Email()])
     username = StringField('Username', validators=[DataRequired()])
     picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
-    print(username)
-    print(picture)
-    aes = cyberSecurity("secret")
-    
-    aes.encryptFile("C://Users//maniv//workspace//Cyber-Secure//cyber//static//profile_pics//test.txt")
+    #print(username)
+#    print(picture)
+    print("Hurray")
+
+    if os.path.isfile("C://Users//maniv//workspace//Cyber-Secure//cyber//static//profile_pics//test.txt"):
+        print("ENCRYPT")
+        aes.encryptFile("C://Users//maniv//workspace//Cyber-Secure//cyber//static//profile_pics//test.txt")
+    else:
+        print("DECRYPT")
+        aes.decryptFile("C://Users//maniv//workspace//Cyber-Secure//cyber//static//profile_pics//test.txt.enc")
+
+    print("DONE")
     submit = SubmitField('Update')
 
     def validate_email(self, field):
